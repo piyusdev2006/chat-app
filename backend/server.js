0ecfccbf20bd343c8e0ba0d9677bb07d05a4fef1
Navi2006 <piyushsingh955583@gmail.com>
2024-06-16 22:12:35 +0530

    final completion

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-// import path from "path";
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,11 +8,13 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 app.use(express.json()); //to parse the incoming requests with JSON payload (from req.body)
 app.use(cookieParser());
@@ -21,12 +23,20 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 // app.get("/", (req, res) => {
 //   // root route http://localhost:5000/
 //     res.send(`Server is ready! ${PORT}`);
 // });
 
-app.listen(5000, () => {
+server.listen(5000, () => {
   connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
 });
