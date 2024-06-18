db3815d7611ecd2752bae3cfac31b8e626c2458a
Navi2006 <piyushsingh955583@gmail.com>
2024-06-18 14:02:03 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("https://mern-chat-application-9uhl.onrender.com/", {
+            const socket = io("http://localhost:5000", {
                 query: {
                     userId: authUser._id,
                 },
